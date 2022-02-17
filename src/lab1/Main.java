542a88ca06301adf352aff035e9532d306c86986
21604 <21604@1k129s4.dune.edu>
2022-02-17 15:23:40 +0200

    OOP

---
+++ src/lab1/Main.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Main {
+
+    public static void libnic() {
+        double chislo = 0;
+        for (double i = 1; i < 10000; i += 1) {
+            if (i % 2 == 0) {
+                chislo -= 1 / i;
+            } else {
+                chislo += 1 / i;
+            }
+        }
+
+        System.out.print(+chislo);
+    }
+
+    public static void main(String[] args) {
+        libnic();
+    }
+}
