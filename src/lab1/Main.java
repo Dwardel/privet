1bb5ec017a843738f1ab6da83268da9d22ba3749
21604 <21604@1k129s4.dune.edu>
2022-03-10 15:04:07 +0200

    haahahahah

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -15,6 +15,12 @@ public class Main {
         System.out.print(+chislo);
     }
 /////////
+    
+    
+    
+    
+    
+    ///
     public static void main(String[] args) {
         libnic();
     }
